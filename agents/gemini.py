1dcb32b12b7baf7dd4c71ab6f5c9999ae1c42374
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2025-03-14 11:29:38 -0300

    chore: gemini name to google chat

--- agents/gemini.py
+++ agents/gemini.py
@@ -9,6 +9,7 @@ GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 googleai_client = ChatGoogleGenerativeAI(
     api_key=GOOGLE_API_KEY,
     model="gemini-2.0-flash-lite",
+    name="gemini",
 )
 
 google_embedding = GoogleGenerativeAIEmbeddings(
